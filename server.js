fd202d2b53ecdc956a9a4a9a84676da674ef3076
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-05-13 06:37:25 -0400

    Created db querys

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express()
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded( { extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+// db.query('SELECT * FROM candidates', (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query('SELECT * FROM candidates WHERE id = 1', (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// // Delete a candidate
+// db.query('DELETE FROM candidates WHERE id =?', 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = 'INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)';
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 // Default response for any other request(Not Found)
 app.use((req, res) => {
